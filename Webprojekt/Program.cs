95087a80456fbe4430c751e16b895cabaa136bd9
LucasLadjevardi <lucladjevardi@gmail.com>
2023-08-10 14:39:40 +0200

    User Authentication

--- Webprojekt/Program.cs
+++ Webprojekt/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDataProtection();
 builder.Services.AddHttpClient();
 
 builder.WebHost.ConfigureKestrel(options =>
